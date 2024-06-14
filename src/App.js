2069a47f89c150328fe4e8bcdae7a6632b379698
prknezek <prknezek@gmail.com>
2024-06-13 21:15:37 -0400

    feat: Add tutorials dropdown and Flashcards link to the main navigation

--- src/App.js
+++ src/App.js
@@ -8,16 +8,23 @@ function App() {
         <a className='navbar-brand' href='#'>
           <img src={require('./logo-inverted.png')} width='80' height='50' className='d-inline-block align-top ms-2' alt='' />
         </a>
-        {/* Tutorials Dropdown */}
-        <div class="dropdown me-auto">
-          <a class="nav-link" href="#" id="tutorialDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <span class="h6 dropdown-toggle text-dark">Tutorials</span>
+        {/* Main nav links */}
+        <div className='me-auto d-flex'>
+          {/* Tutorials Dropdown */}
+          <div class="dropdown me-auto">
+            <a class="nav-link" href="#" id="tutorialDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <span class="h6 dropdown-toggle text-dark">Tutorials</span>
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="tutorialDropdown">
+              <li><a class="dropdown-item" href="#">Linked Lists</a></li>
+              <li><a class="dropdown-item" href="#">Arrays</a></li>
+              <li><a class="dropdown-item" href="#">Sorting</a></li>
+            </ul>
+          </div>
+          {/* Flashcards */}
+          <a class='nav-link'>
+            <span className='h6 text-dark'>Flashcards</span>
           </a>
-          <ul class="dropdown-menu" aria-labelledby="tutorialDropdown">
-            <li><a class="dropdown-item" href="#">Linked Lists</a></li>
-            <li><a class="dropdown-item" href="#">Arrays</a></li>
-            <li><a class="dropdown-item" href="#">Sorting</a></li>
-          </ul>
         </div>
         {/* Signup/login div */}
         <div className='d-inline-block'>
