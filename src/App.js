18c69bb948401d6c84154fabdc32e1ff6ca6a7fe
prknezek <prknezek@gmail.com>
2024-06-14 18:22:29 -0400

    Set up Bootstrap grid system on main screen and moved navbar contents to the center

--- src/App.js
+++ src/App.js
@@ -6,56 +6,60 @@ import { faShuffle, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 function App() {
   return (
     <div className="App">
-      {/* Navbar */}
-      <nav className='navbar fixed-top navbar-light bg-light border-bottom border-2 text-dark'>
-        <a className='navbar-brand' href='#'>
-          <img src={require('./logo-inverted.png')} width='80' height='50' className='d-inline-block align-top ms-2' alt='' />
-        </a>
-        {/* Main nav links */}
-        <div className='me-auto d-flex align-items-center'>
-          {/* Dashboard */}
-          <a class='nav-link' href='#'>
-            <span className='h6 text-dark'>Dashboard</span>
-          </a>
-          {/* Tutorials Dropdown */}
-          <div class="dropdown me-auto">
-            <a class="nav-link" href="#" id="tutorialDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <span class="h6 dropdown-toggle text-dark">Lessons</span>
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="tutorialDropdown">
-              <li class='d-flex justify-content-end me-2'>
-                <button class='position-absolute btn btn-light'>
-                  <FontAwesomeIcon icon={faShuffle} />
-                </button>
-              </li>
-              <li><a class='dropdown-item' href='#'>All Lessons</a></li>
-              <li><a class="dropdown-item" href="#">Saved</a></li>
-              <li><a class="dropdown-item" href="#">Data Structures</a></li>
-              <li><a class="dropdown-item" href="#">Algorithms</a></li>
-            </ul>
+      <div className='container-fluid p-0'>
+        {/* Navbar */}
+        <nav className='navbar navbar-light bg-light border-bottom border-2 text-dark'>
+          <div className='row w-100'>
+            <div className='col-1'></div>
+            <div className='col-10'>
+              <div className='d-flex align-items-center'>
+                {/* Logo */}
+                <a className='navbar-brand' href='#'>
+                  <img src={require('./logo-inverted.png')} width='80' height='50' className='d-inline-block align-top ms-2' alt='' />
+                </a>
+                {/* Main nav links */}
+                <div className='d-flex me-auto align-items-center'>
+                  <a className='nav-link' href='#'>
+                    <span className='h6 text-dark'>Dashboard</span>
+                  </a>
+                  <div className='dropdown'>
+                    <a class="nav-link" href="#" id="tutorialDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      <span class="h6 dropdown-toggle text-dark">Lessons</span>
+                    </a>
+                    <ul className='dropdown-menu' aria-labelledby='tutorialDropdown'>
+                      <li className='d-flex justify-content-end me-2'>
+                        <button className='position-absolute btn btn-light'>
+                          <FontAwesomeIcon icon={faShuffle} />
+                        </button>
+                      </li>
+                      <li><a className='dropdown-item' href='#'>All Lessons</a></li>
+                      <li><a className='dropdown-item' href='#'>Saved</a></li>
+                      <li><a className='dropdown-item' href='#'>Data Structures</a></li>
+                      <li><a className='dropdown-item' href='#'>Algorithms</a></li>
+                    </ul>
+                  </div>
+                  <a className='nav-link' href='#'>
+                    <span className='h6 text-dark'>Flashcards</span>
+                  </a>
+                </div>
+                {/* Search bar */}
+                <form className='d-flex align-items-center mt-1 me-2'>
+                  <input id='search-input' className='form-control border-0 shadow-none' type='search' placeholder='Search lessons' aria-label='Search'></input>
+                  <button type='submit' className='btn shadow-none px-2'>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                  </button>
+                </form>
+                {/* Signup/login div */}
+                <div className='d-flex'>
+                  <button className='btn btn-light shadow-none' id='signup'>Sign Up</button>
+                  <button className='btn btn-light shadow-none' id='login'>Log in</button>
+                </div>
+              </div>
+            </div>
+            <div className='col-1'></div>
           </div>
-          {/* Flashcards */}
-          <a class='nav-link' href='#'>
-            <span className='h6 text-dark'>Flashcards</span>
-          </a>
-        </div>
-        {/* Search bar */}
-        <form class='d-flex align-items-center mt-1 me-3'>
-            <input id='search-input' class="form-control border-0 shadow-none" type="search" placeholder="Search lessons" aria-label="Search"></input>
-            <button type='submit' class='btn shadow-none px-2'>
-              <FontAwesomeIcon icon={faMagnifyingGlass} />
-            </button>
-          </form>
-        {/* Signup/login div */}
-        <div className='d-flex me-3'>
-          <button class='btn btn-light shadow-none' id='signup'>
-            Sign Up
-          </button>
-          <button class='btn btn-light shadow-none' id='login'>
-            Log in
-          </button>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
   );
 }
