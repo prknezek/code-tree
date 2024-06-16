8e0946577175f943c897860cc582a6bdd73c73b9
prknezek <prknezek@gmail.com>
2024-06-15 21:43:12 -0400

    Improved slides' UI and UX

--- src/LessonSwiper/LessonSwiper.js
+++ src/LessonSwiper/LessonSwiper.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {Swiper, SwiperSlide} from 'swiper/react';
+import { isDesktop } from 'react-device-detect';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -9,7 +10,8 @@ import './lessonswiper.css';
 
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
-const numSlides = 6;
+const numSlides = 5;
+const colors = ['#ef476f', '#ffd166', '#06d6a0', '#118ab2', '#c8b6ff'];
 
 export default function LessonSwiper() {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -21,9 +23,8 @@ export default function LessonSwiper() {
   return (
     <>
       <Swiper 
-        slidesPerView={3}
         loop={true}
-        speed={2000}
+        speed={1200}
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
@@ -33,16 +34,39 @@ export default function LessonSwiper() {
         pagination={{
           clickable: true,
         }}
-        navigation={true}
         onSlideChange={handleSlideChange}
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
+        breakpoints={
+          {
+            480: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+            900: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            1280: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            }
+          }
+        }
       >
         {/* Create numSlides SwiperSlide elements */}
         {[...Array(numSlides).keys()].map((index) => (
           <SwiperSlide 
             key={index} 
-            className={index === activeIndex ? 'scale-up-center' : 'scale-down-center'}>
+            className={
+              `${isDesktop ? 'swiper-no-swiping' : ''}
+               ${index === activeIndex ? 'scale-up-center' : 'scale-down-center'}
+               lesson-slide`
+            }
+            style={{ backgroundColor: colors[index], userSelect: 'none'}}
+          >
+            <h1>Slide {index + 1}</h1>
+            <h1>Hello</h1>
           </SwiperSlide>
         ))}
       </Swiper>
