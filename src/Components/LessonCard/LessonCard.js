5b31ff469860e0ed0e0b13c7a7462d58ff310b02
prknezek <prknezek@gmail.com>
2024-06-22 18:01:40 -0500

    Updated the styling of LessonCards to match the theme of the website

--- src/Components/LessonCard/LessonCard.js
+++ src/Components/LessonCard/LessonCard.js
@@ -5,31 +5,24 @@ import './lessonCard.css';
 class LessonCard extends React.Component {
   render() {
     return (
-      <div class="card p-3 mb-2 text-dark col-4">
-        <div class="d-flex justify-content-between">
-          <div class="d-flex flex-row align-items-center">
-            <div class="icon"> <i class="bx bxl-mailchimp"></i> </div>
-              <div class="ms-2 c-details">
-                <h6 class="mb-0">Mailchimp</h6> <span>1 days ago</span>
-              </div>
+      <div className='card p-3 text-dark' style={{width: '35rem'}}>
+        <div className='d-flex justify-content-between'>
+          <div className='d-flex align-items-center'>
+            <div className='icon'></div>
+            <div className='ms-3 m-0'>
+              <h2 className='card-title'>{this.props.title}</h2>
             </div>
-            <div class="badge"> <span className='px-5'>Design</span> </div>
-        </div>
-        <div class="mt-5">
-          <h3 class="card-title">SUCKER Product<br></br>Designer-Singapore</h3>
-          <div class="mt-5">
-            <div class="progress">
-              <div class="progress-bar" role="progressbar" style={{width: '50%'}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-            <div class="mt-3"> <span class="text1">32 Applied <span class="text2">of 50 capacity</span></span> </div>
           </div>
+          <div className='badge'><span>{this.props.category}</span></div>
+        </div>
+        <div className='card-body pb-0'>
+          <p className='card-text'>{this.props.description}</p>
+          <div className="progress">
+            <div className="progress-bar" role="progressbar" style={{width: `${this.props.completion}%`}} aria-valuenow={this.props.completion} aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <div className='text-muted'><span className='text1'>{this.props.completion + '% complete'}</span></div>
         </div>
       </div>
-      // <div className="lesson-card">
-      //   <h2>{this.props.title}</h2>
-      //   <p>{this.props.description}</p>
-      //   <p>{this.props.date}</p>
-      // </div>
     );
   }
 }
--- src/pages/LessonHome/LessonHome.js
+++ src/pages/LessonHome/LessonHome.js
@@ -3,7 +3,12 @@ import LessonCard from "../../components/LessonCard/LessonCard";
 export default function LessonHome() {
   return (
     <div>
-      <LessonCard />
+      <LessonCard 
+        title='Linked List' 
+        category='Algorithm'
+        description='A linked list is a linear data structure, in which the elements are not stored at contiguous memory locations. The elements in a linked list are linked using pointers.'
+        completion='65'
+      />
     </div>
   );
 }
