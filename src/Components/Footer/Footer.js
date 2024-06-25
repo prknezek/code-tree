905c955ef4eb3559deaa1658d102fa6b502d7c04
prknezek <prknezek@gmail.com>
2024-06-24 20:50:29 -0500

    Added favorite option to lessonCard component

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Footer() {
   return (
@@ -6,9 +7,9 @@ export default function Footer() {
       <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <p class="col-md-4 mb-0 text-body-secondary">© 2024 LearnDSA</p>
 
-        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <NavLink to="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
           <img src={require('../../assets/imgs/logo-dark.png')} className='bi me-2' width={100} height={62}></img>
-        </a>
+        </NavLink>
 
         <ul class="nav col-md-4 justify-content-end">
           <li class="nav-item"><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
--- src/Components/LessonCard/LessonCard.js
+++ src/Components/LessonCard/LessonCard.js
@@ -1,47 +1,53 @@
-import React from 'react';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
+import { faArrowRight, faStar } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
 
 import './lessonCard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-class LessonCard extends React.Component {
-  render() {
-    const { title, category, description, completion, lessonId } = this.props;
+const LessonCard = (props) => {
+  const { title, category, description, completion, lessonId } = props;
+  const [isFavorite, setIsFavorite] = useState(false);
 
-    const progressBarStyle = {
-      width: `${completion}%`,
-      backgroundColor: completion === '100' ? '#0BDA51' : '', // Change the progress bar color to green if completion is 100%
-    };
+  const progressBarStyle = {
+    width: `${completion}%`,
+    backgroundColor: completion === '100' ? '#0BDA51' : '', // Change the progress bar color to green if completion is 100%
+  };
 
-    return (
-      <div className='card p-3 text-dark shadow-pop-bl' id='card-container'>
-        <div className='d-flex justify-content-between flex-sm-row flex-column'>
-          <div className='d-flex align-items-center'>
-            <div className='icon'>
-              <img src={require('../../assets/icons/linked-list.svg').default} width={'40px'} alt='Linked List' />
-            </div>
-            <h2 className='card-title ms-3 m-0'>{title}</h2>
+  const handleFavoriteClick = () => {
+    setIsFavorite(!isFavorite);
+  };
+
+  return (
+    <div className='card p-3 text-dark shadow-pop-bl' id='card-container'>
+      <div className='d-flex justify-content-between flex-sm-row flex-column'>
+        <div className='d-flex align-items-center'>
+          <div className='icon'>
+            <img src={require('../../assets/icons/linked-list.svg').default} width={'40px'} alt='Linked List' />
           </div>
-          <div className='mt-2'>
-            <div className='badge'><span>{category}</span></div>
-          </div>
+          <h2 className='card-title ms-3 m-0'>{title}</h2>
         </div>
-        <div className='card-body pb-0'>
-          <p className='card-text'>{description}</p>
-          <div className="progress">
-            <div className="progress-bar" role="progressbar" style={progressBarStyle} aria-valuenow={completion} aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
-          <div className='text-muted'><span className='text1'>{completion + '% complete'}</span></div>
-          <div className='w-100 d-flex align-items-center'>
-            <NavLink to={`${lessonId}`} className='ms-auto btn btn-light'>
-              Go to lesson <FontAwesomeIcon icon={faArrowRight} className='ps-2'/>
-            </NavLink>
-          </div>
+        <div className='mt-2 d-flex align-items-center'>
+          <div className='badge'><span>{category}</span></div>
+          <button className='favorite-button' onClick={handleFavoriteClick}>
+            <FontAwesomeIcon icon={faStar} className={`star-icon ${isFavorite ? 'favorite' : ''}`} />
+          </button>
         </div>
       </div>
-    );
-  }
+      <div className='card-body pb-0'>
+        <p className='card-text'>{description}</p>
+        <div className="progress">
+          <div className="progress-bar" role="progressbar" style={progressBarStyle} aria-valuenow={completion} aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <div className='text-muted'><span className='text1'>{completion + '% complete'}</span></div>
+        <div className='w-100 d-flex align-items-center'>
+          <NavLink to={`${lessonId}`} className='ms-auto btn btn-light'>
+            Go to lesson <FontAwesomeIcon icon={faArrowRight} className='ps-2'/>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LessonCard;
