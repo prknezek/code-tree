75906acad4307551f5f8a1498490cad16e637dd3
prknezek <prknezek@gmail.com>
2024-06-27 21:23:26 -0400

    Made LessonHome page and LessonCard more responsive

--- src/Components/LessonCard/LessonCard.js
+++ src/Components/LessonCard/LessonCard.js
@@ -19,31 +19,35 @@ const LessonCard = (props) => {
   };
 
   return (
-    <div className='card p-3 text-dark shadow-pop-bl' id='card-container'>
-      <div className='d-flex justify-content-between flex-sm-row flex-column'>
-        <div className='d-flex align-items-center'>
-          <div className='icon'>
-            <img src={require(`../../assets/icons/${img_url}`)} width={'40px'} alt='Linked List' />
+    <div className='col-lg-4 col-md-6 col-12 mt-4'>
+      <div className='card p-4 text-dark shadow-pop-bl' id='card-container'>
+        <div className='d-flex justify-content-between flex-column'>
+          <div className='d-flex align-items-center' id='card-header'>
+            <div className='icon'>
+              <img src={require(`../../assets/icons/${img_url}`)} width={'40px'} alt='Linked List' />
+            </div>
+            <h2 className='card-title ms-3 m-0'>{title}</h2>
           </div>
-          <h2 className='card-title ms-3 m-0'>{title}</h2>
+          <div className='mt-3 d-flex align-items-center' id='card-subheader'>
+            <div className='badge'><span>{category}</span></div>
+            <button className='favorite-button' onClick={handleFavoriteClick}>
+              <FontAwesomeIcon icon={faStar} className={`star-icon ${isFavorite ? 'favorite' : ''}`} />
+            </button>
+          </div>
         </div>
-        <div className='mt-2 d-flex align-items-center'>
-          <div className='badge'><span>{category}</span></div>
-          <button className='favorite-button' onClick={handleFavoriteClick}>
-            <FontAwesomeIcon icon={faStar} className={`star-icon ${isFavorite ? 'favorite' : ''}`} />
-          </button>
-        </div>
-      </div>
-      <div className='card-body pb-0'>
-        <p className='card-text'>{description}</p>
-        <div className="progress">
-          <div className="progress-bar" role="progressbar" style={progressBarStyle} aria-valuenow={completion} aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
-        <div className='text-muted'><span className='text1'>{completion + '% complete'}</span></div>
-        <div className='w-100 d-flex align-items-center'>
-          <NavLink to={`${lessonId}`} className='ms-auto btn btn-light'>
-            Go to lesson <FontAwesomeIcon icon={faArrowRight} className='ps-2'/>
-          </NavLink>
+        <div className='card-body pb-0'>
+          <div id='card-description'>
+            <p className='card-text'>{description}</p>
+          </div>
+          <div className="progress mt-2">
+            <div className="progress-bar" role="progressbar" style={progressBarStyle} aria-valuenow={completion} aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <div className='text-muted'><span className='text1'>{completion + '% complete'}</span></div>
+          <div className='w-100 d-flex align-items-center' id='card-link-container'>
+            <NavLink to={`${lessonId}`} className='ms-auto btn btn-light' id='card-link'>
+              Go to lesson <FontAwesomeIcon icon={faArrowRight} className='ps-2'/>
+            </NavLink>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/LessonHome/LessonHome.js
+++ src/pages/LessonHome/LessonHome.js
@@ -2,23 +2,33 @@ import LessonCard from "../../components/LessonCard/LessonCard";
 
 export default function LessonHome() {
   return (
-    <div className='w-100 d-flex flex-lg-row flex-column p-lg-4 p-3 justify-content-between'>
-      <LessonCard 
-        title='Linked List' 
-        category='Data Structure'
-        description='A linked list is a linear data structure, in which the elements are not stored at contiguous memory locations. The elements in a linked list are linked using pointers.'
-        completion='100'
-        lessonId='linked-list'
-        img_url='linked-list.svg'
-      />
-      <LessonCard 
-        title='Arrays' 
-        category='Data Structure'
-        description='An array is a collection of items stored at contiguous memory locations. The idea is to store multiple items of the same type together.'
-        completion='35'
-        lessonId='arrays'
-        img_url='linked-list.svg'
-      />
+    <div className='container'>
+      <div className='row p-4 pt-0'>
+        <LessonCard 
+          title='Linked List' 
+          category='Data Structure'
+          description=''
+          completion='100'
+          lessonId='linked-list'
+          img_url='linked-list.svg'
+        />
+        <LessonCard 
+          title='Arrays' 
+          category='Data Structure'
+          description=''
+          completion='35'
+          lessonId='arrays'
+          img_url='linked-list.svg'
+        />
+        <LessonCard 
+          title='Arrays' 
+          category='Data Structure'
+          description=''
+          completion='35'
+          lessonId='arrays'
+          img_url='linked-list.svg'
+        />
+      </div>
     </div>
   );
 }
